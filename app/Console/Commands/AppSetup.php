c2099293ecd26a2c2187e8ee471e5b60bf994f3c
Onojeta Brown <onosbrown.saved@gmail.com>
2024-11-18 08:50:07 +0100

    deploy local

---
+++ app/Console/Commands/AppSetup.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Storage;
+
+class AppSetup extends Command
+{
+
+    protected $signature = 'app:setup';
+    public function handle()
+    {
+        $ac = env('ACCES_CODE', '');
+        $email = env('ADMIN_EMAIL', 'onosbrown.saved@gmail.com');
+        $password = env('ADMIN_PASSWORD', '#1414bruno#');
+
+        Storage::put('.app_installed', $ac);
+        Storage::put('.user_email', $email);
+        Storage::put('.user_pass', $password);
+
+        $this->info('Setup completed');
+        return 0;
+    }
+}
